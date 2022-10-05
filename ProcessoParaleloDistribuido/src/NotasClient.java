720fbe0018dd0d322d9525734a22a4f704b57c11
ArthurFoltz <78622379+ArthurFoltz@users.noreply.github.com>
2022-10-05 20:38:01 -0300

    Atividade Lab 02
    5/10/2022

---
+++ ProcessoParaleloDistribuido/src/NotasClient.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+
+public class NotasClient {
+
+	public static void main(String[] args) throws IOException {
+		if	(args.length != 2) {
+			System.out.println("Uso: java NotasClient <arthur> <none>");
+			System.exit(1);
+		}
+		// cria um socket datagrama
+		DatagramSocket socket = new DatagramSocket();
+		// envia um pacote
+		byte[] nome = new byte[256];
+		nome = args[1].getBytes();
+		InetAddress servIP = InetAddress.getByName(args[0]);
+		DatagramPacket pacEnv = new DatagramPacket(nome, nome.length, servIP, 0x8003);
+		socket.send(pacEnv);
+		// obtem a resposta
+		byte[] bufNota = new byte[8];
+		DatagramPacket pacRec = new DatagramPacket(bufNota, bufNota.length);
+		socket.receive(pacRec);
+		// mostra a resposta
+		System.out.println("Nota do aluno '"+args[1]+"' = "+ByteBuffer.wrap(bufNota).getDouble());
+		
+		// fecha o socket
+		socket.close();
+	}
+
+
+}
